9513cbed5b3aba502d67e6f58806ad185b7f0136
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-08 14:26:05 +0200

    Cleanup and fix compilation warning

--- src/jason-tickets-beans.ads
+++ src/jason-tickets-beans.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  jason-tickets-beans -- Beans for module tickets
---  Copyright (C) 2016 Stephane.Carrez
+--  Copyright (C) 2016, 2017 Stephane.Carrez
 --  Written by Stephane.Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,15 +25,12 @@ with Util.Beans.Objects;
 with AWA.Tags.Beans;
 with Jason.Tickets.Modules;
 with Jason.Tickets.Models;
-with Jason.Projects.Models;
 with Jason.Projects.Beans;
 package Jason.Tickets.Beans is
 
    type Ticket_Bean is new Jason.Tickets.Models.Ticket_Bean with record
-      Module     : Jason.Tickets.Modules.Ticket_Module_Access := null;
-      --  Project_Id : ADO.Identifier := ADO.NO_IDENTIFIER;
-      Ticket_Id  : ADO.Identifier := ADO.NO_IDENTIFIER;
-      --  Project    : Jason.Projects.Models.Project_Ref;
+      Module        : Jason.Tickets.Modules.Ticket_Module_Access := null;
+      Ticket_Id     : ADO.Identifier := ADO.NO_IDENTIFIER;
       Project       : Jason.Projects.Beans.Project_Bean_Access;
 
       --  List of tags associated with the wiki page.
@@ -82,7 +79,6 @@ package Jason.Tickets.Beans is
    --  ------------------------------
    type Ticket_List_Bean is new Jason.Tickets.Models.Ticket_List_Bean with record
       Module        : Jason.Tickets.Modules.Ticket_Module_Access := null;
-      --  Project       : Jason.Projects.Models.Project_Ref;
       Project       : Jason.Projects.Beans.Project_Bean_Access;
 
       --  List of tickets.
